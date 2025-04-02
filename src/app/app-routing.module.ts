a7b4dca0c7959fa7039a78e2b4d8fe07515de55c
TE109 <1991tmce@gmail.com>
2025-04-02 11:14:12 -0400

    Prerender false for vercel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,9 @@ import { MissionfilterComponent } from './SpaceX/missionfilter/missionfilter.com
 import { MissiondetailsComponent } from './SpaceX/missiondetails/missiondetails.component';
 
 const routes: Routes = [
-  { path: "missionlist", component: MissionlistComponent },
-  { path: "missionfilter", component: MissionfilterComponent },
-  { 
-    path: "missiondetails/:flight_number", 
-    component: MissiondetailsComponent,
-    data: { renderMode: 'no-prerender' } 
-  }
-];
+  {path:"missionlist",component:MissionlistComponent},
+{path:"missionfilter",component:MissionfilterComponent},
+{path:"missiondetails/:flight_number",component:MissiondetailsComponent,data: { flight_number: 1 }}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
